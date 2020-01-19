a87e5cc694466e6b2471d34818af72b44236061d
Papa-Pedro <54333730+Papa-Pedro@users.noreply.github.com>
2020-01-19 23:46:20 +0400

    Code Change

---
+++ MachanismsOOP/Number1/NumberOneViewController.swift
@@ -0,0 +1,65 @@
+//
+//  NumberOneViewController.swift
+//  MachanismsOOP
+//
+//  Created by TimFerens on 10.01.20.
+//  Copyright © 2020 TimFerens. All rights reserved.
+//
+
+import UIKit
+
+class NumberOneViewController: UIViewController {
+    
+    var size: Int = 0
+    var determinant: Int?
+    var array: [[Int]] = []
+    
+    
+    @IBOutlet weak var titulDeterminantLabel: UILabel!
+    @IBOutlet weak var determinantLabel: UILabel!
+    @IBOutlet weak var sizeMatrixLabel: UILabel!
+    @IBOutlet weak var sizeMatrixField: UITextField!
+    @IBAction func sizeMatrixField(_ sender: Any) {
+        sizeMatrixField.text = ""
+    }
+    @IBOutlet weak var inputSizeButton: UIButton!
+    
+    @IBAction func inputSizeButton(_ sender: UIButton) {
+        if let size1: Int = Int(sizeMatrixField.text ?? "") {
+            if size1 < 5 {
+                if size1 <= 0 {
+                    sizeMatrixLabel.text = "Не бывает таких матриц"
+                } else {
+                    size = size1
+                    performSegue(withIdentifier: "ShowSegue", sender: self)
+                }
+            }
+            else {
+                sizeMatrixLabel.text = "Большой размер для тестового задания"
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if determinant != nil {
+            titulDeterminantLabel.isHidden = false
+            determinantLabel.text = "\(String(describing: determinant!) )"
+            sizeMatrixField.text = "\(size)"
+        } else {
+            titulDeterminantLabel.isHidden = true
+            determinantLabel.text = ""
+            sizeMatrixField.text = "___"
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowSegue"
+        {
+            let destinationNC = segue.destination as! UINavigationController
+            let destinationVC = destinationNC.topViewController as! ArrayCollectionViewController
+            destinationVC.size = size
+        }
+    }
+}
+
